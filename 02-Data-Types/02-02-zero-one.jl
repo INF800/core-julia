bddcaf35a6e7f16366f319dd5bf657cf4690aa5f
Asapanna Rakesh <rakeshark22@gmail.com>
2021-01-11 01:43:33 +0530

    float :100:

---
+++ 02-Data-Types/02-02-zero-one.jl
@@ -0,0 +1,17 @@
+println(zero(Float32))
+println(zero(Float64))
+println(zero(Int128))
+println(one(Float32))
+println(one(Float64))
+
+println(zero(BigInt))
+println(zero(BigFloat))
+
+#: 0.0
+#: 0.0
+#: 0
+#: 1.0
+#: 1.0
+
+#: 0
+#: 0.0
